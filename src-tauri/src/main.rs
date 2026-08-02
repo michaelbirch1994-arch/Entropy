395a69ddc83d8e39c97b8595eec23eee45ae095f
michaelbirch1994-arch <michaelbirch1994@gmail.com>
2026-08-02 16:09:44 -0400

    Add files via upload

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -23,6 +23,8 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![grant_folder_scope])
         .run(tauri::generate_context!())
         .expect("error while running Entropy");
