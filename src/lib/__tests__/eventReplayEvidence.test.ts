33cbad0ea07b3c885b037152bbca0662757b9175
michaelbirch1994-arch <michaelbirch1994@gmail.com>
2026-08-19 17:14:37 -0400

    Use realistic replay cadence in snapshot fixture

--- src/lib/__tests__/eventReplayEvidence.test.ts
+++ src/lib/__tests__/eventReplayEvidence.test.ts
@@ -19,7 +19,11 @@ function fight(): ReplayFightEntry {
           profession: "Guardian",
           inSquad: true,
           isCommander: true,
-          points: [{ t: 0, x: 0, y: 0 }, { t: 20_000, x: 0, y: 0 }],
+          points: [
+            { t: 0, x: 0, y: 0 },
+            { t: 10_000, x: 0, y: 0 },
+            { t: 20_000, x: 0, y: 0 },
+          ],
           downIntervals: [],
           deadIntervals: [],
           facings: [],
@@ -31,7 +35,11 @@ function fight(): ReplayFightEntry {
           profession: "Warrior",
           inSquad: true,
           isCommander: false,
-          points: [{ t: 0, x: 100, y: 0 }, { t: 20_000, x: 300, y: 0 }],
+          points: [
+            { t: 0, x: 100, y: 0 },
+            { t: 10_000, x: 200, y: 0 },
+            { t: 20_000, x: 300, y: 0 },
+          ],
           downIntervals: [[9_000, 11_000]],
           deadIntervals: [],
           facings: [],
@@ -42,7 +50,11 @@ function fight(): ReplayFightEntry {
         {
           id: "enemy-1",
           name: "Enemy",
-          points: [{ t: 0, x: 50, y: 50 }, { t: 20_000, x: 50, y: 50 }],
+          points: [
+            { t: 0, x: 50, y: 50 },
+            { t: 10_000, x: 50, y: 50 },
+            { t: 20_000, x: 50, y: 50 },
+          ],
           downIntervals: [[9_500, 10_500]],
           deadIntervals: [],
           facings: [],
