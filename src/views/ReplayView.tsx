79b7e45169177808c7048e967652b1db35bb1b5f
michaelbirch1994-arch <michaelbirch1994@gmail.com>
2026-08-20 04:49:05 -0400

    Surface Intelligence anchors above replay workspace

--- src/views/ReplayView.tsx
+++ src/views/ReplayView.tsx
@@ -1,1 +1,11 @@
-export { default } from "./ReplayViewV2";
+import ReplayIntelligenceAnchorRail from "../components/replay/ReplayIntelligenceAnchorRail";
+import ReplayViewV2 from "./ReplayViewV2";
+
+export default function ReplayView() {
+  return (
+    <div className="space-y-4">
+      <ReplayIntelligenceAnchorRail />
+      <ReplayViewV2 />
+    </div>
+  );
+}
