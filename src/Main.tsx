5ee8439ced1ece8dfd118338b8e1d6e60db3bab9
michaelbirch1994-arch <michaelbirch1994@gmail.com>
2026-08-24 18:19:53 -0400

    Pass 8: load acceptance corrections after finish layers

--- src/Main.tsx
+++ src/Main.tsx
@@ -15,6 +15,7 @@ import "./Styles/ObsidianGold.css";
 import "./Styles/BlackGoldFinish.css";
 import "./Styles/BlackGoldRefinement.css";
 import "./Styles/RoyalPolish.css";
+import "./Styles/ObsidianGoldPass8.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -23,4 +24,3 @@ createRoot(document.getElementById("root")!).render(
     </ThemeProvider>
   </StrictMode>
 );
-
